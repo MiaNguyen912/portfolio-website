56180657c6321cd5604daa3fe111518bf53474dd
MiaNguyen912 <thuydn7@uci.edu>
2024-10-24 16:13:03 -0700

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,15 +7,29 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mia Nguyen",
-  description: "Portfolio of Mia Nguyen",
+  title: "Mia Nguyen Portfolio",
+  description: "Explore the portfolio of Mia Nguyen, a full-stack developer specializing in modern web technologies.",
+  keywords: "Mia Nguyen, portfolio, front-end developer, full-stack developer, web developer, Next.js, React.js",
+  author: "Mia Nguyen",
+  openGraph: {
+    title: "Mia Nguyen Portfolio",
+    description: "The portfolio of Mia Nguyen, showcasing front-end development projects and skills.",
+    url: "https://mia-portfolio-flame.vercel.app/",
+    siteName: "Mia Nguyen Portfolio",
+    locale: "en_US",
+    type: "website",
+    viewport: 'width=device-width, initial-scale=1.0', 
+  },
+  robots: {
+    index: true, // Allow page to be indexed in search results
+    follow: true, // // Allow search engine to follow and index links on page
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      {/* <script src="https://kit.fontawesome.com/b088c22a54.js" crossOrigin="anonymous"></script> */}
     </html>
   );
 }
